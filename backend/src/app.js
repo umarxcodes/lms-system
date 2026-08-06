40f0f253192b12a7621366a8f7e53735633d2d2a
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-08-06 11:01:10 +0500

    Update README with project documentation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,10 @@
 import express from "express";
-import authRoutes from "./modules/auth/auth.routes.js";
+
 
 const app = express();
 
 app.use(express.json());
-app.use("/api/auth", authRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Api is Working Fine !");
